cc612e0ebb0dff8575c867c5b49335e49947304c
Juan Galvis <juan.galvis>
2024-03-13 07:44:26 -0500

    Repository Information

--- src/main/java/org/proyectocrud/repository/Repository.java
+++ src/main/java/org/proyectocrud/repository/Repository.java
@@ -1,4 +1,13 @@
 package org.proyectocrud.repository;
 
-public interface Repository {
+import java.util.List;
+
+public interface Repository <T> {
+    List<T> findAll();
+
+    T getById(Integer id);
+
+    void save(T t);
+
+    void delete(Integer id);
 }
