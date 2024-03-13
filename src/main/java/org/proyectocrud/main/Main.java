d18a460a8db35c94d1a5dc2bdae6e6f6d04ca5b5
Juan Galvis <juan.galvis>
2024-03-13 08:14:15 -0500

    Changing connection and repository info

--- src/main/java/org/proyectocrud/main/Main.java
+++ src/main/java/org/proyectocrud/main/Main.java
@@ -1,18 +1,17 @@
 package org.proyectocrud.main;
 
+import org.proyectocrud.model.Employee;
+import org.proyectocrud.repository.EmployeeRepository;
+import org.proyectocrud.repository.Repository;
 import org.proyectocrud.util.DatabaseConnection;
 
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        try (Connection connection = DatabaseConnection.getInstance();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery("SELECT * FROM employees");){
-
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("first_name") + ", " + resultSet.getString("email"));
-            }
+        try (Connection connection = DatabaseConnection.getInstance()) {
+            Repository<Employee> repository = new EmployeeRepository();
+            repository.findAll().forEach(System.out::println);
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("La conexion a la base de datos falló");
