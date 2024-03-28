c0e7662c1c8062c3d97e9a7192812cfcbdbbff36
Juan Galvis <juan.galvis>
2024-03-28 12:08:53 -0500

    Add more logic to the code to see the magic of Transactions

--- src/main/java/org/proyectocrud/main/Main.java
+++ src/main/java/org/proyectocrud/main/Main.java
@@ -4,54 +4,46 @@ import org.proyectocrud.model.Employee;
 import org.proyectocrud.repository.EmployeeRepository;
 import org.proyectocrud.repository.Repository;
 import org.proyectocrud.util.DatabaseConnection;
-import org.proyectocrud.view.SwingApp;
 
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        SwingApp swingApp = new SwingApp();
-        swingApp.setVisible(true);
+        try (Connection connection = DatabaseConnection.getInstance()) {
 
-        /* try (Connection connection = DatabaseConnection.getInstance()) {
-            Repository<Employee> repository = new EmployeeRepository();
-            repository.findAll().forEach(System.out::println);
+            if (connection.getAutoCommit()) {
+                connection.setAutoCommit(false);
+            }
 
-            System.out.println("-- Insertando un empleado --");
-            Employee employee = new Employee();
-            employee.setFirst_name("Camila");
-            employee.setPa_surname("Riano");
-            employee.setMa_surname("Zarate");
-            employee.setEmail("camilariano@gmail.com");
-            employee.setSalary(24000F);
+            try {
+                Repository<Employee> repository = new EmployeeRepository(connection);
 
-            repository.save(employee);
+                System.out.println("-- Insertado un nuevo cliente --");
 
-            System.out.println("--- Nuevo empleado insertado ---");
-            repository.findAll().forEach(System.out::println);
+                Employee e = new Employee();
 
-            System.out.println("--- Actualizando o Insertando empleado --");
-            Integer idEmployee = 8;
-            Employee employeeUpdated = new Employee();
-            employeeUpdated.setFirst_name("JuanCamilo");
-            employeeUpdated.setPa_surname("Galvis");
-            employeeUpdated.setMa_surname("Cuellar");
-            employeeUpdated.setEmail("juancagalvis@gmail.com");
-            employeeUpdated.setSalary(16000F);
+                e.setFirst_name("Carlos");
+                e.setPa_surname("Lopez");
+                e.setMa_surname("Villa");
+                e.setEmail("carl222@example.com");
+                e.setSalary(300F);
+                e.setCurp("MECjkl987guihio214");
+                repository.save(e);
+                connection.commit();
 
-            repository.update(idEmployee, employeeUpdated);
-            System.out.println("Empleado actualizado");
-            System.out.println(repository.getById(idEmployee));
+                e.setFirst_name("David");
+                e.setPa_surname("Lopez");
+                e.setMa_surname("Villa");
+                e.setEmail("davlop@example.com");
+                e.setSalary(300F);
+                e.setCurp("MECjkl987guihio215");
+                repository.save(e);
+                connection.commit();
 
-            System.out.println("Empleado a eliminar con id: 9");
-            repository.delete(9);
-
-            System.out.println("------ Lista de Usuarios ------");
-            repository.findAll().forEach(System.out::println);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("La conexion a la base de datos falló");
-        }*/
+            } catch (SQLException sqlError) {
+                connection.rollback();
+                throw new RuntimeException(sqlError);
+            }
+        }
     }
 }
--- src/main/java/org/proyectocrud/repository/EmployeeRepository.java
+++ src/main/java/org/proyectocrud/repository/EmployeeRepository.java
@@ -1,7 +1,6 @@
 package org.proyectocrud.repository;
 
 import org.proyectocrud.model.Employee;
-import org.proyectocrud.util.DatabaseConnection;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -9,13 +8,15 @@ import java.util.List;
 
 public class EmployeeRepository implements Repository<Employee> {
 
-    private Connection getConnection() throws SQLException {
-        return DatabaseConnection.getInstance();
+    private Connection connection;
+
+    public EmployeeRepository(Connection connection) {
+        this.connection = connection;
     }
     @Override
     public List<Employee> findAll() throws SQLException {
         List<Employee> employees = new ArrayList<>();
-        try(Statement statement = getConnection().createStatement();
+        try(Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM employees")) {
             while(resultSet.next()) {
                 Employee e = createEmployee(resultSet);
@@ -28,7 +29,7 @@ public class EmployeeRepository implements Repository<Employee> {
     @Override
     public Employee getById(Integer id) throws SQLException {
         Employee employee = null;
-        try (PreparedStatement preparedStatement = getConnection().prepareStatement("SELECT * FROM employees WHERE id = ?")) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM employees WHERE id = ?")) {
             preparedStatement.setInt(1, id);
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 if (resultSet.next()) {
@@ -41,14 +42,14 @@ public class EmployeeRepository implements Repository<Employee> {
 
     @Override
     public void save(Employee employee) throws SQLException {
-        String sql = "INSERT INTO employees (first_name, pa_surname, ma_surname, email, salary) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO employees (first_name, pa_surname, ma_surname, email, salary, curp) VALUES (?, ?, ?, ?, ?, ?)";
         insertOrUpdateEmployee(employee, sql);
     }
 
     @Override
     public void update(Integer id, Employee employee) throws SQLException {
         if (getById(id) != null) {
-            String sql = "UPDATE employees SET first_name = ?, pa_surname = ?, ma_surname = ?, email = ?, salary = ? WHERE id = " + id;
+            String sql = "UPDATE employees SET first_name = ?, pa_surname = ?, ma_surname = ?, email = ?, salary = ?, curp= ? WHERE id = " + id;
             insertOrUpdateEmployee(employee, sql);
             System.out.println("Empleado existe, actualización realizada");
         } else {
@@ -58,12 +59,13 @@ public class EmployeeRepository implements Repository<Employee> {
     }
 
     private void insertOrUpdateEmployee(Employee employee, String sql) throws SQLException {
-        try (PreparedStatement preparedStatement = getConnection().prepareStatement(sql)) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, employee.getFirst_name());
             preparedStatement.setString(2, employee.getPa_surname());
             preparedStatement.setString(3, employee.getMa_surname());
             preparedStatement.setString(4, employee.getEmail());
             preparedStatement.setFloat(5, employee.getSalary());
+            preparedStatement.setString(6, employee.getCurp());
 
             preparedStatement.executeUpdate();
         }
@@ -72,7 +74,7 @@ public class EmployeeRepository implements Repository<Employee> {
     @Override
     public void delete(Integer id) throws SQLException {
         String sql = "DELETE FROM employees WHERE id = ?";
-        try (PreparedStatement preparedStatement = getConnection().prepareStatement(sql)) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setInt(1, id);
             preparedStatement.executeUpdate();
         }
@@ -87,6 +89,7 @@ public class EmployeeRepository implements Repository<Employee> {
         employee.setMa_surname(resultSet.getString("ma_surname"));
         employee.setEmail(resultSet.getString("email"));
         employee.setSalary(resultSet.getFloat("salary"));
+        employee.setCurp(resultSet.getString("curp"));
         return employee;
     }
 }
