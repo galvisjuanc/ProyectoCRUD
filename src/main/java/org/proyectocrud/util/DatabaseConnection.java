4e2e57e9a7ab575618aba3c34e926065b26e06e4
Juan Galvis <juan.galvis>
2024-03-29 10:48:54 -0500

    Change DB connection using apache commons dbcp2

--- src/main/java/org/proyectocrud/util/DatabaseConnection.java
+++ src/main/java/org/proyectocrud/util/DatabaseConnection.java
@@ -1,5 +1,7 @@
 package org.proyectocrud.util;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,12 +11,24 @@ public class DatabaseConnection {
     private static String url = "jdbc:mysql://localhost:3306/project";
     private static String root = "root";
     private static String pw = "BrokenYouth_1711*";
-    private static Connection connection;
+    private static BasicDataSource pool;
 
-    public static Connection getInstance() throws SQLException {
-        if (connection == null) {
-            connection = DriverManager.getConnection(url, root, pw);
+    public static BasicDataSource getInstance() {
+        if (pool == null) {
+            pool = new BasicDataSource();
+            pool.setUrl(url);
+            pool.setUsername(root);
+            pool.setPassword(pw);
+
+            pool.setInitialSize(3);
+            pool.setMinIdle(3);
+            pool.setMaxIdle(10);
+            pool.setMaxTotal(10);
         }
-        return connection;
+        return pool;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return getInstance().getConnection();
     }
 }
